269adaab8a74efcb8758cf1d32b9d691a4808e6a
Ariel Perez <perezariel1192@gmail.com>
2023-02-06 08:24:36 -0300

    chat header ui done

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,9 @@
-import { Avatar } from '@mui/material'
+import MoreVert from '@mui/icons-material/MoreVert'
+import SearchOutlined from '@mui/icons-material/SearchOutlined'
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import { Avatar, IconButton } from '@mui/material'
 import React, { useEffect, useState } from 'react'
+import './Chat.css'
 
 function Chat() {
   const [seed, setSeed] = useState("")
@@ -9,6 +13,7 @@ function Chat() {
     }, [])
 
   return (
+
     <div className='chat'>
         <div className='chat__header'>
           <Avatar src={`https://api.dicebear.com/5.x/miniavs/svg?seed=${seed}`} />
@@ -16,7 +21,19 @@ function Chat() {
             <h3>Room name</h3>
             <p>Last seen at ...</p>
           </div>
+          <div className='chat__headerRight'>
+            <IconButton>
+              <SearchOutlined/>
+            </IconButton>
+            <IconButton>
+              <AttachFileIcon />
+            </IconButton>
+            <IconButton>
+              <MoreVert />
+            </IconButton>
+          </div>
         </div>
+
         <div className='chat__body'>
 
         </div>
